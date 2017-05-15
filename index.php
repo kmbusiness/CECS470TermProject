033ee3cace8907beea8ab70772936569308dc2d0
Allen Muy <allenmuy@gmail.com>
2017-05-15 15:59:50 -0700

    allen - changed home page

--- index.php
+++ index.php
@@ -4,35 +4,92 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Chapter 9</title>
+    <title>Home Page</title>
 
     <link rel="stylesheet" href="style.css">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet">
+    <script src="https://use.fontawesome.com/1eb48d54b5.js"></script>
 
   </head>
   <body>
     <?php include 'dentist-nav.php' ?>
 
-    <div class="container">
-      <p>
-			Are you looking for a dentist that will really care for your teeth unlike other dentists? One that provides quality service at affordable prices?
-			If you said yes to those question, then John Nguyen D.M.D is the dentist for you. Dr. John is a certified dentist from the American Dental
-			Association and will turn your frown upside down.
-		</p>
+    <div class="dentist-banner">
+      <div class="dentist-signup">
+        <div class="log">
+          <h2>Sign Into Your <br> Account!</h2>
+          <p><i>Don't have an account?</i><br>
+            <button type="submit" class="signupbtn">SIGN UP!</button>
+          </p>
 
-		<h2> Patient Reviews </h2>
-		<p>
-			"Had a root canal that was so painful every time I went to see my regular dentists, they only made the pain worse. Not Dr. John.
-			 I'm glad I stopped by his office and highly recommend everyone to visit him." - Frederick V.
-			 <br/>
-			 <br/>
-			 "My children love going to his office more than any other, he is so good with kids. Staff was very friendly and helpful too."
-			 - Canai N.
-		</p>
+        </div>
+        <div class="log">
+          <?php include 'loginform.php' ?>
+        </div>
+
+        <!-- <form action="">
+          <div class="container-signup">
+            <label><b>Email</b></label>
+            <input type="text" placeholder="Enter Email" name="email" required>
+
+            <label><b>Password</b></label>
+            <input type="password" placeholder="Enter Password" name="psw" required>
+            <div class="clearfix">
+              <button type="button" class="cancelbtn">Cancel</button>
+              <button type="submit" class="signupbtn">Sign In</button>
+            </div>
+          </div>
+        </form> -->
+      </div>
+      <img class="mySlides" src="images/banner1.png">
+      <img class="mySlides" src="images/banner2.png">
+      <img class="mySlides" src="images/banner3.png">
+    </div>
+
+    <div class="container">
+      <section id="dentist-info">
+        <div class="first">
+          <a href="#">
+          <i class="fa fa-user-md fa-5x"></i>
+      		<h2>ABOUT US</h2></a>
+      	</div>
+        <div class="second">
+          <a href="#">
+          <i class="fa fa-heartbeat fa-5x"></i>
+      		<h2>LEARN</h2></a>
+      	</div>
+        <div class="third">
+          <a href="#">
+          <i class="fa fa-hospital-o fa-5x"></i>
+      		<h2>CONTACT</h2></a>
+      	</div>
+      </section>
+    </div>
+    <div class="wrapper">
+      <section id="dentist-service">
+        <div class="left_col">
+          <div>
+            <a href="#"><img src="images/services.png" height="200px" width="200px"></a>
+            <h2>SERVICES</h2>
+            <p>Learn more about our what we do!</p>
+            <a href="#"><i class="fa fa-arrow-circle-right fa-3x" aria-hidden="true"></i></a>
+          </div>
+
+        </div>
+        <div class="right_col">
+          <div>
+            <a href="#"><img src="images/appointments.png" height="200" width="200"></a>
+            <h2>APPOINTMENTS</h2>
+            <p>Schedule your appointment here!</p>
+            <a href="#"><i class="fa fa-arrow-circle-right fa-3x" aria-hidden="true"></i></a>
+          </div>
+        </div>
+      </section>
     </div>
 
 
    <?php include 'dentist-footer.php' ?>
 
+   <script src="functions.js"></script>
   </body>
 </html>
--- loginform.php
+++ loginform.php
@@ -1,9 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
+
 	<?php
 		session_start();
 		$_SESSION['prev_page'] = 'loginform.php';
@@ -16,14 +11,13 @@
 			$errors = str_split($e);
 			// print_r($errors);
 		}
-		
+
 	?>
 
 	<form action="login.php" method="post">
-		<form action="furnitureorder.php" method="post" id="order-form">
-			<div>
-				<label for="email">Customer Email</label><br/>
-				<input id = "email" type="email" name="email" size="60" class="required" />
+			<div class="container-signup">
+				<label for="email">Email</label><br/>
+				<input id="email" type="email" name="email" class="required" />
 				<?php
 				if($errors[0] == 1) {
 					echo '
@@ -33,33 +27,31 @@
 					';
 				}
 				?>
-			</div>
-			<div>
-				<label for="pw">Password</label><br/>
-				<input id = "pw" type="text" name="pw" size="60" class="required" />
-				<?php
-				if($errors[1] == 1) {
-					echo '
-					<div class="error-warning" style="color:red;">
-					Missing password.
-					</div>
-					';
-				}
-				?>
-			</div>
-			<?php
-			if($errors[2] == 1) {
-				echo '
-				<div class="error-warning" style="color:red;">
-				Login error: Check email and password.
-				</div>
-				';
-			}
-			?>
-			<br>
 
-       		<input type="submit"  value="Login">
+					<label for="pw">Password</label><br/>
+					<input id="pw" type="text" name="pw" class="required" />
+					<?php
+					if($errors[1] == 1) {
+						echo '
+						<div class="error-warning" style="color:red;">
+						Missing password.
+						</div>
+						';
+					}
+					?>
+
+					<?php
+					if($errors[2] == 1) {
+						echo '
+						<div class="error-warning" style="color:red;">
+						Login error: Check email and password.
+						</div>
+						';
+					}
+					?>
+					<br>
+					<div class="clearfix">
+       			<button type="submit" class="signinbtn">LOG IN</button>
+					</div>
+			</div>
 		</form>
-	</form>
-</body>
-</html>
