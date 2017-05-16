7438744e2ca895a2db38cff9a0a4f8c954fa5b85
Vinnie Vu <vinnievu25@yahoo.com>
2017-05-15 18:38:28 -0700

    update

--- dentist-footer.php
+++ dentist-footer.php
@@ -1,4 +1,8 @@
 <!-- Footer -->
+<link rel="stylesheet" href="style.css">
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet">
+<script src="https://use.fontawesome.com/1eb48d54b5.js"></script>
+
 <footer>
 	<p>
 		Contact information: <a href="mailto:someone@example.com">someone@example.com</a>.
