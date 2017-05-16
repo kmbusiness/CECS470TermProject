3cd4d8dc26b228ba0d1b4b48d900528ffb434c8d
Vinnie Vu <vinnievu25@yahoo.com>
2017-05-15 18:10:54 -0700

    Index and footer updated

--- dentist-footer.php
+++ dentist-footer.php
@@ -1,6 +1,22 @@
 <!-- Footer -->
 <footer>
-<p>Contact information: <a href="mailto:someone@example.com">
-someone@example.com</a>.</p>
+	<p>
+		Contact information: <a href="mailto:someone@example.com">someone@example.com</a>.
+		<br/>
+		<a href="privacy.php"> Our Privacy Policy </a>
+		<br/>
+		<a href="index.php"> Home </a>
+		<br/>
+		<a href="aboutJohn.php"> About Us </a>
+		<br/>
+		<a href="contact.php"> Contact Us </a>
+		<br/>
+		<a href="learn.php"> Learn About Taking Care of Your Teeth </a>
+		<br/>
+		<a href="procedure.php"> Our Services </a>
+		<br/>
+		
+	</p>
+	
 
 </footer>
