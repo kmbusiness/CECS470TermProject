79d51415f5dfbe6af17ccf03e2b84b21b665caec
Allen Muy <allenmuy@gmail.com>
2017-05-17 00:15:07 -0700

    allen

--- loginform.php
+++ loginform.php
@@ -12,11 +12,11 @@
 			// print_r($errors);
 		}
 
-	?>
+	?> 
 
 	<form action="login.php" method="post">
 			<div class="container-signup">
-				<label for="email">Email</label>
+				<label for="email"><i class="fa fa-envelope" aria-hidden="true"></i> Email</label>
 				<?php
 				if($errors[0] == 1) {
 					echo '
@@ -28,19 +28,19 @@
 				?>
 				<br/>
 				<input id="email" type="email" name="email" class="required" />
-				
 
-					<label for="pw">Password</label><?php
+
+					<label for="pw"><i class="fa fa-lock" aria-hidden="true"></i> Password</label><?php
 					if($errors[1] == 1) {
 						echo '
 						<span class="error-warning" style="color:red;float:right;">
-						Missing password. 
+						Missing password.
 						</span>
 						';
 					}
 					?>
 
-					
+
 					<br/>
 					<input id="pw" type="password" name="pw" class="required" />
 					<?php
@@ -52,7 +52,7 @@
 						';
 					}
 					?>
-					
+
 					<br>
 					<div class="clearfix">
        			<button type="submit" class="signinbtn">LOG IN</button>
